1dc0f865d72215553758cb777c8266201b1982e5
r616on <r616on@gmail.com>
2021-12-05 02:12:59 +0300

    first_Task_Done

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/eoatrkskcmi4kr1x?svg=true)](https://ci.appveyor.com/project/r616on/react-home-works-7-hoc)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import FirstTask from "./components/FirstTask/FirstTask";
+import SecondTask from "./components/SecondTask/SecondTask";
+
+function App() {
+  return (
+    <div className="App">
+      <FirstTask />
+      {/* <span>Раздел Компонентов</span>
+      <SecondTask /> */}
+    </div>
+  );
+}
+
+export default App;
