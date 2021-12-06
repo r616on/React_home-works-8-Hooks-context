15c6ea8decce63e1b87533f7e6839e4c5875e887
r616on <r616on@gmail.com>
2021-12-06 23:48:16 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import FirstTask from "./components/FirstTask/FirstTask";
-import SecondTask from "./components/SecondTask/SecondTask";
 
 function App() {
   return (
     <div className="App">
       <FirstTask />
-      <SecondTask />
     </div>
   );
 }
